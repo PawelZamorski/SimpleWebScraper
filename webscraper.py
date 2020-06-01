b14243d3f5683f9e2aaf6eaeaf852bf73331e12d
Pawel Zamorski <zamorskipawel@wp.pl>
2020-06-01 13:31:54 +0100

    Get data from home page

--- webscraper.py
+++ webscraper.py
@@ -3,5 +3,27 @@ import requests as req
 # https://pypi.org/project/beautifulsoup4/
 from bs4 import BeautifulSoup
 
+# get page
+page = req.get('http://www.konwersatoriummuzyczne.pl')
+# parse
+soup = BeautifulSoup(page.content, 'html.parser')
+# indent HTML
+# pretty_soup = soup.prettify()
+# print
+# print(pretty_soup)
 
-print('By')
+# 1. Get data from home page
+# get part of HTML
+id_oferta = soup.find(id='oferta')
+#get all elements having givn class
+class_pbc = id_oferta.find_all('div', class_='pricing-block-content') # Returns ResultSet (list)
+# add data as js object
+data_home = []
+for element in class_pbc:
+    heading = element.h3.text
+    detail = element.p.text
+    link = element.a['href']
+    img = element.img['src']
+    item = {'heading': heading, 'detail': detail, 'link': link, 'img': img}
+    data_home.append(item)
+
